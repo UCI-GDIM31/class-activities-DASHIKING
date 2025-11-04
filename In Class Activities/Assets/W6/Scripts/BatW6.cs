ee2f9edee53eff5883e883b627ccaa7cdc9b3ee3
DASHIKING <jbeiduo@gmail.com>
2025-11-03 19:12:55 -0800

    Update BatW6.cs
    1

--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -3,5 +3,33 @@ using UnityEngine;
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
+    [SerializeField] private float _speed = 2.0f;
+    [SerializeField] private Transform _target;
+
+
+    void Update()
+    {
+        
+        if (_target != null)
+        {
+            
+            Vector3 direction = (_target.position - transform.position).normalized;
+
+            
+            transform.position += direction * _speed * Time.deltaTime;
+        }
+    }
+
     
+    public void StartChasing()
+    {
+        gameObject.SetActive(true);
+    }
+
+    
+    public void StopChasing()
+    {
+        gameObject.SetActive(false);
+    }
 }
+
